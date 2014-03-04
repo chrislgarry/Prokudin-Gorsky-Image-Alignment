b8d7fdbdfe9911508f62762bb03b7ba9b8d56893
Chris Garry <chrislgarry@gmail.com>
2014-03-03 21:47:38 -0500

    Update distributionFromImage.m

--- distributionFromImage.m
+++ distributionFromImage.m
@@ -1,5 +1,5 @@
 function [bin_proportions] = distributionFromImage(image, num_bins)
-%% Map pixel values in image layer into n bins based on value
+% Map pixel values in image layer into n bins based on value
     image_linear = reshape(image,1,numel(image));
     bin_proportions = histc(image_linear,0:(256/num_bins):256);
     bin_proportions = bin_proportions(1:end-1)/numel(image);
